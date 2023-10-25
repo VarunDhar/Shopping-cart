5e8faaf10cc0a9e6b3a1215fff4f4f3361008996
VarunDhar <85066986+VarunDhar@users.noreply.github.com>
2023-10-26 01:04:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+  const {cart} = useSelector((state)=>state);
   return (
     <div >
     <nav className="navbar"><div className='name'><NavLink to="/"><h2>ShopperPoint</h2></NavLink></div>
       <div className='rightside'><NavLink to="/"><p>Home</p></NavLink>
-          <NavLink to="/cart"><p id="cart">🛒</p></NavLink>
+          <NavLink to="/cart"><p className="cart">🛒</p></NavLink>
+          {cart.length>0 &&(<span className='cartSize'>{cart.length}</span>)}
       </div></nav>
       
     
